34d46030d332beda99e4a77e0371357be6753aae
semz-ui <Michaeltimilehin36@gmail.com>
2024-07-05 18:56:48 +0100

    banner issue fixed

--- src/components/GetStarted2.tsx
+++ src/components/GetStarted2.tsx
@@ -9,10 +9,12 @@ const GetStarted2 = () => {
             <div className="w-full sm:w-1/2">
                 <img src={img} />
             </div>
-            <div className="w-full sm:w-1/2 xl:w-1/3 mt-5 sm:mt-0 px-2 sm:px-0">
+            <div className="w-full flex flex-col items-center sm:items-start sm:w-1/2 xl:w-1/3 mt-5 sm:mt-0 px-2 sm:px-0">
                 <h1 className="text-base lg:text-3xl md:text-2xl">Transact on the go</h1>
                 <p className="text-sm md:text-base text-gray-400 mt-2 mb-8">Stay on top of your business with our easy-to-use app</p>
-                <Button title="Get Started" />
+                <div>
+                    <Button title="Get Started" />
+                </div>
                 <div className="mt-5 flex flex-wrap gap-2 ">
                     <img src={appleStoreImage} className="w-24" />
                     <img src={gooplePlayImage} className="w-24" />
--- src/components/IndividualBanner.tsx
+++ src/components/IndividualBanner.tsx
@@ -17,7 +17,7 @@ const IndividualBanner = () => {
             <img src={img3} className="absolute w-40 sm:w-64 lg:w-96 mt-72 sm:mt-80 lg:mt-0 z-10 md:mt-0" />
             <img src={img1} className="absolute w-32 sm:w-40 lg:w-60 mt-[480px] md:mt-64 lg:ml-10 lg:mt-96 z-20" />
             <img src={img2} className="absolute w-24 sm:w-36 z-10 mr-44 sm:mr-60 mt-60 sm:mt-60 lg:mb-96 lg:mr-96 lg:w-56 md:mb-96" />
-            <img src={img4} className="absolute w-16 sm:w-20 md:w-36 lg:w-40 mt-80 sm:mt-96 lg:mt-28 xl:mt-4 mr-44 sm:mr-60 lg:mr-96 md:mb-96" />
+            <img src={img4} className="absolute w-16 sm:w-20 md:w-36 lg:w-40 mt-80 sm:mt-96 lg:mt-28 xl:mt-[480px] mr-44 sm:mr-60 lg:mr-96 md:mb-96" />
         </div>
     </div>;
 };
